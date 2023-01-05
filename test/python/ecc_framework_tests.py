bbb2fb1210cb0f5e9ab3161e1ad55ed41f491c3b
Thomas Grurl <thomas.grurl@fh-hagenberg.at>
2023-01-05 22:28:19 +0100

    initial draft for tests

---
+++ test/python/ecc_framework_tests.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from unittest import mock
+import argparse
+import mqt.qecc.ecc_framework_qiskit_wrapper
+
+
+@mock.patch('argparse.ArgumentParser.parse_args',
+            return_value=argparse.Namespace(m="D", p=0.001, n=1000, s=1, f="ghz02.qasm", e=None,
+                                            fs="aer_simulator_extended_stabilizer", ecc="Q7Steane", fq=0))
+def test_command(mock_args):
+    mqt.qecc.ecc_framework_qiskit_wrapper.main()
+
+
+if __name__ == "__main__":
+    test_command()
