d78ad84f9b4caa0f26bdbb9d0b1413adc070f686
Thomas Grurl <thomas.grurl@fh-hagenberg.at>
2023-01-05 22:33:35 +0100

    adding type annotation

--- test/python/ecc_framework_tests.py
+++ test/python/ecc_framework_tests.py
@@ -8,7 +8,7 @@ import mqt.qecc.ecc_framework_qiskit_wrapper
 @mock.patch('argparse.ArgumentParser.parse_args',
             return_value=argparse.Namespace(m="D", p=0.001, n=1000, s=1, f="ghz02.qasm", e=None,
                                             fs="aer_simulator_extended_stabilizer", ecc="Q7Steane", fq=0))
-def test_command(mock_args):
+def test_command(mock_args) -> None:
     mqt.qecc.ecc_framework_qiskit_wrapper.main()
 
 
