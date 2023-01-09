f0889bf38e2d7621d06ea0d6adf7d32c50dd5c1e
Lukas Burgholzer <lukas.burgholzer@jku.at>
2023-01-09 22:01:11 +0100

    🚨 another set of linter fixes
    Signed-off-by: Lukas Burgholzer <lukas.burgholzer@jku.at>

--- test/test_utils.cpp
+++ test/test_utils.cpp
@@ -8,11 +8,7 @@
 
 #include <gtest/gtest.h>
 
-class UtilsTest : public testing::TestWithParam<std::string> {
-protected:
-    void setUp() {
-    }
-};
+class UtilsTest : public testing::TestWithParam<std::string> {};
 
 TEST(UtilsTest, MatConversion) {
     auto ctxx = flint::nmodxx_ctx(2);
@@ -48,33 +44,33 @@ TEST(UtilsTest, MatConversionBack) {
 }
 
 TEST(UtilsTest, TestSwapRows) {
-    gf2Mat matrix = {{1, 1, 0, 1, 0, 0, 1},
-                     {1, 0, 1, 0, 1, 0, 0},
-                     {0, 1, 1, 0, 0, 1, 0}};
-    gf2Mat sol    = {{1, 1, 0, 1, 0, 0, 1},
-                     {0, 1, 1, 0, 0, 1, 0},
-                     {1, 0, 1, 0, 1, 0, 0}};
+    gf2Mat       matrix = {{1, 1, 0, 1, 0, 0, 1},
+                           {1, 0, 1, 0, 1, 0, 0},
+                           {0, 1, 1, 0, 0, 1, 0}};
+    const gf2Mat sol    = {{1, 1, 0, 1, 0, 0, 1},
+                           {0, 1, 1, 0, 0, 1, 0},
+                           {1, 0, 1, 0, 1, 0, 0}};
 
     Utils::swapRows(matrix, 1, 2);
     EXPECT_TRUE(sol == matrix);
 }
 
 TEST(UtilsTest, TestReduce) {
-    gf2Mat matrix = {{1, 1, 0, 1, 0, 0, 1},
-                     {1, 0, 1, 0, 1, 0, 0},
-                     {0, 1, 1, 0, 0, 1, 0}};
-    gf2Mat sol    = {{1, 0, 1, 0, 1, 0, 0},
-                     {0, 1, 1, 0, 0, 1, 0},
-                     {0, 0, 0, 1, 1, 1, 1}};
-    auto   res    = Utils::gauss(matrix);
+    const gf2Mat matrix = {{1, 1, 0, 1, 0, 0, 1},
+                           {1, 0, 1, 0, 1, 0, 0},
+                           {0, 1, 1, 0, 0, 1, 0}};
+    const gf2Mat sol    = {{1, 0, 1, 0, 1, 0, 0},
+                           {0, 1, 1, 0, 0, 1, 0},
+                           {0, 0, 0, 1, 1, 1, 1}};
+    auto         res    = Utils::gauss(matrix);
     EXPECT_TRUE(Utils::getFlintMatrix(sol) == res);
 }
 
 TEST(UtilsTest, TestTranspose) {
-    gf2Mat matrix  = {{1, 0, 0, 1, 0, 1, 1},
-                      {0, 1, 0, 1, 1, 0, 1},
-                      {0, 0, 1, 0, 1, 1, 1}};
-    gf2Mat matrixT = {
+    const gf2Mat matrix  = {{1, 0, 0, 1, 0, 1, 1},
+                            {0, 1, 0, 1, 1, 0, 1},
+                            {0, 0, 1, 0, 1, 1, 1}};
+    const gf2Mat matrixT = {
             {1, 0, 0},
             {0, 1, 0},
             {0, 0, 1},
@@ -86,10 +82,10 @@ TEST(UtilsTest, TestTranspose) {
 }
 
 TEST(UtilsTest, TestTranspose2) {
-    gf2Mat matrix  = {{1, 0, 0, 1, 0, 1, 1},
-                      {0, 1, 0, 1, 1, 0, 1},
-                      {0, 0, 1, 0, 1, 1, 1}};
-    gf2Mat matrixT = {
+    const gf2Mat matrix  = {{1, 0, 0, 1, 0, 1, 1},
+                            {0, 1, 0, 1, 1, 0, 1},
+                            {0, 0, 1, 0, 1, 1, 1}};
+    const gf2Mat matrixT = {
             {1, 0, 0},
             {0, 1, 0},
             {0, 0, 1},
@@ -101,125 +97,125 @@ TEST(UtilsTest, TestTranspose2) {
 }
 
 TEST(UtilsTest, GaussGF2testNotInRS) {
-    gf2Mat matrix = {{1, 0, 0, 1, 0, 1, 1},
-                     {0, 1, 0, 1, 1, 0, 1},
-                     {0, 0, 1, 0, 1, 1, 1}};
+    const gf2Mat matrix = {{1, 0, 0, 1, 0, 1, 1},
+                           {0, 1, 0, 1, 1, 0, 1},
+                           {0, 0, 1, 0, 1, 1, 1}};
 
-    gf2Vec vector = {0, 0, 0, 1, 0, 1, 1};
+    const gf2Vec vector = {0, 0, 0, 1, 0, 1, 1};
 
     auto isInRowSpace = Utils::isVectorInRowspace(matrix, vector);
     EXPECT_FALSE(isInRowSpace);
 }
 
 TEST(UtilsTest, GaussGF2testNotInRS2) {
-    gf2Mat matrix2 = {{1, 0, 0, 0},
-                      {0, 1, 0, 0},
-                      {0, 0, 1, 0}};
-    gf2Vec vector2 = {1, 1, 1, 1};
+    const gf2Mat matrix2 = {{1, 0, 0, 0},
+                            {0, 1, 0, 0},
+                            {0, 0, 1, 0}};
+    const gf2Vec vector2 = {1, 1, 1, 1};
 
     auto isInRowSpace2 = Utils::isVectorInRowspace(matrix2, vector2);
     EXPECT_FALSE(isInRowSpace2);
 }
 
 TEST(UtilsTest, GaussGF2testInRS) {
-    gf2Mat matrix = {{1, 0, 0, 1, 0, 1, 1},
-                     {0, 1, 0, 1, 1, 0, 1},
-                     {0, 0, 1, 0, 1, 1, 1}};
+    const gf2Mat matrix = {{1, 0, 0, 1, 0, 1, 1},
+                           {0, 1, 0, 1, 1, 0, 1},
+                           {0, 0, 1, 0, 1, 1, 1}};
 
-    gf2Vec vector       = {1, 0, 0, 1, 0, 1, 1};
-    auto   isInRowSpace = Utils::isVectorInRowspace(matrix, vector);
+    const gf2Vec vector       = {1, 0, 0, 1, 0, 1, 1};
+    auto         isInRowSpace = Utils::isVectorInRowspace(matrix, vector);
     EXPECT_TRUE(isInRowSpace);
 }
 
 TEST(UtilsTest, GaussGF2testInRS2) {
-    gf2Mat matrix2       = {{1, 0, 0, 0},
-                            {0, 1, 0, 0},
-                            {0, 0, 1, 0}};
-    gf2Vec vector2       = {1, 1, 1, 0};
-    auto   isInRowSpace2 = Utils::isVectorInRowspace(matrix2, vector2);
+    const gf2Mat matrix2       = {{1, 0, 0, 0},
+                                  {0, 1, 0, 0},
+                                  {0, 0, 1, 0}};
+    const gf2Vec vector2       = {1, 1, 1, 0};
+    auto         isInRowSpace2 = Utils::isVectorInRowspace(matrix2, vector2);
     EXPECT_TRUE(isInRowSpace2);
 }
 
 TEST(UtilsTest, GaussGF2testNotInRSTrivial) {
-    gf2Mat matrix = {{1, 0, 0, 1, 0, 1, 1},
-                     {0, 1, 0, 1, 1, 0, 1},
-                     {0, 0, 1, 0, 1, 1, 1}};
+    const gf2Mat matrix = {{1, 0, 0, 1, 0, 1, 1},
+                           {0, 1, 0, 1, 1, 0, 1},
+                           {0, 0, 1, 0, 1, 1, 1}};
 
-    gf2Vec vector       = {1, 1, 1, 1, 1, 1, 1};
-    auto   isInRowSpace = Utils::isVectorInRowspace(matrix, vector);
+    const gf2Vec vector       = {1, 1, 1, 1, 1, 1, 1};
+    auto         isInRowSpace = Utils::isVectorInRowspace(matrix, vector);
     EXPECT_FALSE(isInRowSpace);
 }
 
 TEST(UtilsTest, GaussGF2testInRSTrivial) {
-    gf2Mat matrix = {{1, 0, 0, 1, 0, 1, 1},
-                     {0, 1, 0, 1, 1, 0, 1},
-                     {0, 0, 1, 0, 1, 1, 1}};
+    const gf2Mat matrix = {{1, 0, 0, 1, 0, 1, 1},
+                           {0, 1, 0, 1, 1, 0, 1},
+                           {0, 0, 1, 0, 1, 1, 1}};
 
-    gf2Vec vector       = {0, 0, 0, 0, 0, 0, 0};
-    auto   isInRowSpace = Utils::isVectorInRowspace(matrix, vector);
+    const gf2Vec vector       = {0, 0, 0, 0, 0, 0, 0};
+    auto         isInRowSpace = Utils::isVectorInRowspace(matrix, vector);
     EXPECT_TRUE(isInRowSpace);
 }
 
 TEST(UtilsTest, LinEqSolvTest) {
-    gf2Mat matrix = {{1, 0, 0, 1, 0, 1, 1},
-                     {0, 1, 0, 1, 1, 0, 1},
-                     {0, 0, 1, 0, 1, 1, 1}};
+    const gf2Mat matrix = {{1, 0, 0, 1, 0, 1, 1},
+                           {0, 1, 0, 1, 1, 0, 1},
+                           {0, 0, 1, 0, 1, 1, 1}};
 
-    gf2Vec vector   = {0, 1, 0};
-    auto   res      = Utils::solveSystem(matrix, vector);
-    gf2Vec solution = {0, 1, 0, 0, 0, 0, 0};
+    const gf2Vec vector   = {0, 1, 0};
+    auto         res      = Utils::solveSystem(matrix, vector);
+    const gf2Vec solution = {0, 1, 0, 0, 0, 0, 0};
     std::cout << "res: ";
     Utils::printGF2vector(res);
     EXPECT_TRUE(res == solution);
 }
 
 TEST(UtilsTest, LinEqSolvTest2) {
-    gf2Mat matrix = {{1, 0, 0, 1, 0, 1, 1},
-                     {0, 1, 0, 1, 1, 0, 1},
-                     {0, 0, 1, 0, 1, 1, 1}};
+    const gf2Mat matrix = {{1, 0, 0, 1, 0, 1, 1},
+                           {0, 1, 0, 1, 1, 0, 1},
+                           {0, 0, 1, 0, 1, 1, 1}};
 
-    gf2Vec vector   = {0, 1, 1};
-    auto   res      = Utils::solveSystem(matrix, vector);
-    gf2Vec solution = {0, 1, 1, 0, 0, 0, 0};
+    const gf2Vec vector   = {0, 1, 1};
+    auto         res      = Utils::solveSystem(matrix, vector);
+    const gf2Vec solution = {0, 1, 1, 0, 0, 0, 0};
     std::cout << "sol";
     Utils::printGF2vector(res);
     EXPECT_TRUE(res == solution);
 }
 TEST(UtilsTest, ImportCode) {
-    gf2Mat matrix = {{1, 0, 0, 1, 0, 1, 1},
-                     {0, 1, 0, 1, 1, 0, 1},
-                     {0, 0, 1, 0, 1, 1, 1}};
+    const gf2Mat matrix = {{1, 0, 0, 1, 0, 1, 1},
+                           {0, 1, 0, 1, 1, 0, 1},
+                           {0, 0, 1, 0, 1, 1, 1}};
 
     auto res = Utils::importGf2MatrixFromFile("./resources/codes/testCode.txt");
     EXPECT_TRUE(res == matrix);
 }
 
 TEST(UtilsTest, GetFlintMatrix) {
-    gf2Mat matrix = {{1, 0, 0, 1, 0, 1, 1},
-                     {0, 1, 0, 1, 1, 0, 1},
-                     {0, 0, 1, 0, 1, 1, 1}};
-    auto   s      = Utils::getFlintMatrix(matrix);
+    const gf2Mat matrix = {{1, 0, 0, 1, 0, 1, 1},
+                           {0, 1, 0, 1, 1, 0, 1},
+                           {0, 0, 1, 0, 1, 1, 1}};
+    auto         s      = Utils::getFlintMatrix(matrix);
     print_pretty(s);
 }
 
 TEST(UtilsTest, MatrixMultiply) {
-    gf2Mat matrix = {{1, 0, 0, 1, 0, 1, 1},
-                     {0, 1, 0, 1, 1, 0, 1},
-                     {0, 0, 1, 0, 1, 1, 1}};
-    gf2Vec vec    = {1, 0, 0, 0, 0, 0, 0};
-    gf2Vec sol    = {1, 0, 0};
-    gf2Vec syndr(matrix.size());
+    const gf2Mat matrix = {{1, 0, 0, 1, 0, 1, 1},
+                           {0, 1, 0, 1, 1, 0, 1},
+                           {0, 0, 1, 0, 1, 1, 1}};
+    const gf2Vec vec    = {1, 0, 0, 0, 0, 0, 0};
+    const gf2Vec sol    = {1, 0, 0};
+    gf2Vec       syndr(matrix.size());
     Utils::rectMatrixMultiply(matrix, vec, syndr);
     EXPECT_TRUE(syndr == sol);
 }
 
 TEST(UtilsTest, MatrixMultiply2) {
-    gf2Mat matrix = {{1, 0, 0, 1, 0, 1, 1},
-                     {0, 1, 0, 1, 1, 0, 1},
-                     {0, 0, 1, 0, 1, 1, 1}};
-    gf2Vec vec    = {1, 1, 0, 0, 0, 0, 0};
-    gf2Vec sol    = {1, 1, 0};
-    gf2Vec comp(matrix.size());
+    const gf2Mat matrix = {{1, 0, 0, 1, 0, 1, 1},
+                           {0, 1, 0, 1, 1, 0, 1},
+                           {0, 0, 1, 0, 1, 1, 1}};
+    const gf2Vec vec    = {1, 1, 0, 0, 0, 0, 0};
+    const gf2Vec sol    = {1, 1, 0};
+    gf2Vec       comp(matrix.size());
     Utils::rectMatrixMultiply(matrix, vec, comp);
 
     EXPECT_TRUE(comp == sol);
