918032b16e612c6bfe4e8ae06a0a8ce16c2cece2
lucas <lucberent@gmail.com>
2023-02-01 18:09:36 +0100

    remove python 3.7 support

--- test/app.cpp
+++ test/app.cpp
@@ -6,6 +6,7 @@
 #include "UFDecoder.hpp"
 #include "Utils.hpp"
 
+#include <cmath> /* pow */
 #include <ctime>
 #include <iostream>
 #include <map>
@@ -55,19 +56,20 @@ void runtime(const std::string& codename) {
 }
 
 void decodingPerformance(const double per) {
-    const std::string                          rootPath        = "/home/luca/Documents/codeRepos/qecc/examples/lp_(4,8)-[[1024,18,nan]]_hx.txt";
-    const std::string                          rootPath2       = "/home/luca/Documents/codeRepos/qecc/examples/lp_(4,8)-[[1024,18,nan]]_hz.txt";
-    const std::size_t                          codeK           = 18;
-    const std::size_t                          nrOfRunsPerRate = 1;
+    const std::string                          rootPath  = "/home/luca/Documents/codeRepos/qecc/examples/lp_(4,8)-[[1024,18,nan]]_hx.txt";
+    const std::string                          rootPath2 = "/home/luca/Documents/codeRepos/qecc/examples/lp_(4,8)-[[1024,18,nan]]_hz.txt";
+    const std::size_t                          codeK     = 18;
+    const std::size_t                          nrRuns    = 1000;
     std::map<std::string, double, std::less<>> wordErrRatePerPhysicalErrRate;
 
     auto       code = HGPcode(rootPath, rootPath2, codeK);
     const auto k    = code.getK();
     const auto n    = code.getN();
 
-    std::size_t nrOfFailedRuns = 0U;
-    std::size_t j              = 0;
-    while (nrOfFailedRuns < 1 || j < nrOfRunsPerRate) {
+    std::size_t nrOfFailedRuns   = 0U;
+    std::size_t nrSuccessfulRuns = 0U;
+    std::size_t j                = 0;
+    while (nrOfFailedRuns < 1 || j < nrRuns) {
         auto decoder = std::make_unique<UFDecoder>();
         decoder->setCode(code);
         auto error    = Utils::sampleErrorIidPauliNoise(n, per);
@@ -79,19 +81,21 @@ void decodingPerformance(const double per) {
         auto success = code.isXStabilizer(residualErr);
         if (!success) {
             nrOfFailedRuns++;
+        } else {
+            nrSuccessfulRuns++;
         }
         decoder->reset();
         j++;
     }
     // compute word error rate WER
-    const auto blockErrRate = static_cast<double>(nrOfFailedRuns) / static_cast<double>(j);
-    const auto wordErrRate  = blockErrRate / static_cast<double>(k); // rate of codewords for decoder does not give correct answer (fails or introduces logical operator)
+    const auto logicalErrRate = 1 - static_cast<double>(nrSuccessfulRuns) / static_cast<double>(nrRuns);
+    const auto wordErrRate    = 1.0 - pow(1 - logicalErrRate, (1.0 / codeK)); // rate of codewords for decoder does not give correct answer (fails or introduces logical operator)
     std::cout << "per:wer = " << per << ":" << wordErrRate << std::endl;
     std::cout.flush();
     flint_cleanup();
 }
 
-int main(int argc, char* argv[]) {   // NOLINT(clang-diagnostic-unused-parameter, bugprone-exception-escape,misc-unused-parameters)
-    double per = std::stod(argv[1]); // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+int main(int argc, char* argv[]) {         // NOLINT(clang-diagnostic-unused-parameter, bugprone-exception-escape,misc-unused-parameters)
+    const double per = std::stod(argv[1]); // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
     decodingPerformance(per);
 }
