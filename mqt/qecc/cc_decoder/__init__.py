4e95e376aab03efd08bf53c3d0074a706eca14a4
lucas <lucberent@gmail.com>
2023-03-23 13:50:46 +0100

    fix init.py warnings

--- mqt/qecc/cc_decoder/__init__.py
+++ mqt/qecc/cc_decoder/__init__.py
@@ -1,3 +1,5 @@
-from mqt.qecc.cc_decoder.cli import cli  # noqa: F401
-from mqt.qecc.cc_decoder.comparison import tn_decoder  # noqa: F401
-from mqt.qecc.cc_decoder.hexagonal_color_code import HexagonalColorCode  # noqa: F401
+from mqt.qecc.cc_decoder.cli import cli
+from mqt.qecc.cc_decoder.comparison import tn_decoder
+from mqt.qecc.cc_decoder.hexagonal_color_code import HexagonalColorCode
+
+__all__ = ["cli", "tn_decoder", "HexagonalColorCode"]
