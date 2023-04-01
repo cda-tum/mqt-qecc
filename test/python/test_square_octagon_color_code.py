92047ad1c4360602cdf489309bae9ace8fc649c3
lucas <lucberent@gmail.com>
2023-04-01 13:52:42 +0200

    fix 4.8.8. tests

---
+++ test/python/test_square_octagon_color_code.py
@@ -0,0 +1,33 @@
+"""
+Test the square octagon color code. Created by Peter-Jan Derks.
+"""
+
+from __future__ import annotations
+
+import numpy as np
+import pytest
+from mqt.qecc.cc_decoder.square_octagon_color_code import SquareOctagonColorCode
+
+
+@pytest.fixture(scope="function")
+def code(request) -> SquareOctagonColorCode:
+    """Color code on 4.8.8 lattice."""
+    return SquareOctagonColorCode(distance=request.param)
+
+
+@pytest.mark.parametrize("code", list(range(3, 23, 2)), indirect=True)
+def test_number_of_qubits(code: SquareOctagonColorCode) -> None:
+    """Test the number of qubits for larger distances."""
+    assert len(code.data_qubits) == 1 / 2 * code.distance ** 2 + code.distance - 1 / 2
+    assert len(code.ancilla_qubits) == (1 / 2 * code.distance ** 2 + code.distance - 1 / 2) // 2
+
+
+def test_d3(code: SquareOctagonColorCode) -> None:
+    """Test coordinates of qubits for distance 3."""
+    assert code.data_qubits == {(0, 1), (2, 1), (6, 1), (3, 2), (5, 2), (3, 4), (5, 4)}
+    assert code.ancilla_qubits == {(4, 0), (1, 3), (4, 3)}
+
+
+def test_H(code: SquareOctagonColorCode) -> None:
+    """Test the parity check matrix for distance 3."""
+    assert code.H == np.array([[0, 0, 1, 0, 1, 1, 1], [0, 1, 0, 1, 0, 1, 1], [1, 1, 1, 0, 0, 1, 0]]).all()
