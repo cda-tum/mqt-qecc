3d5f85d07d80f65049cc2708b694c256c2f40056
lucas <lucberent@gmail.com>
2023-11-30 09:39:40 +0100

    fixing mypy ctd

--- test/python/analog_info/test_simulation_utils.py
+++ test/python/analog_info/test_simulation_utils.py
@@ -169,9 +169,9 @@ def test_generate_err() -> None:
     n = 10
     ch = np.ones(n) * p
     channel = np.copy(ch), np.copy(ch), np.copy(ch)
-    residual: list[np.int32] = [np.zeros(n).astype(np.int32), np.zeros(n).astype(np.int32)]
+    residual = [np.zeros(n).astype(np.int32), np.zeros(n).astype(np.int32)]
 
-    expected = np.array([np.zeros(n).astype(np.int32), np.zeros(n).astype(np.int32)])
+    expected = np.array([np.zeros(n).astype(np.float64), np.zeros(n).astype(np.float64)])
     assert np.array_equal(generate_err(n, channel, residual), expected)
 
     residual[0][0] = 1
@@ -277,11 +277,11 @@ def test_err_chnl_setup() -> None:
 
 def test_build_ss_pcm() -> None:
     """Test build_single_stage_pcm function."""
-    h: NDArray[np.int32] = np.array([[1, 1, 0, 0], [0, 1, 1, 0], [0, 0, 1, 1], [1, 0, 0, 1]]).astype(np.int32)
-    m: NDArray[np.int32] = np.array([[1, 1, 0, 0], [0, 1, 1, 0], [0, 0, 1, 1], [1, 0, 0, 1]]).astype(np.int32)
+    h = np.array([[1, 1, 0, 0], [0, 1, 1, 0], [0, 0, 1, 1], [1, 0, 0, 1]])
+    m = np.array([[1, 1, 0, 0], [0, 1, 1, 0], [0, 0, 1, 1], [1, 0, 0, 1]])
     id_r = np.identity(m.shape[1])
     zeros = np.zeros((m.shape[0], h.shape[1]))
-    exp = np.block([[h, id_r], [zeros, m]]).astype(np.int32)
+    exp = np.block([[h, id_r], [zeros, m]])
     assert np.array_equal(build_single_stage_pcm(h, m), exp)
 
 
