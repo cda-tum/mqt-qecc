362bcf7596619b894c15879c6c98fdaeb4d01911
Tom Peham <pehamtom@gmx.at>
2024-06-25 13:37:04 +0200

    Added heuristic_verification_stabilizers to exported module function.

--- src/mqt/qecc/ft_stateprep/__init__.py
+++ src/mqt/qecc/ft_stateprep/__init__.py
@@ -1,7 +1,7 @@
 """Methods for synthesizing fault tolerant state preparation circuits."""
 
 from __future__ import annotations
-from .state_prep import StatePrepCircuit, depth_optimal_prep_circuit, gate_optimal_prep_circuit, heuristic_prep_circuit, gate_optimal_verification_circuit, gate_optimal_verification_stabilizers, heuristic_verification_circuit
+from .state_prep import StatePrepCircuit, depth_optimal_prep_circuit, gate_optimal_prep_circuit, heuristic_prep_circuit, gate_optimal_verification_circuit, gate_optimal_verification_stabilizers, heuristic_verification_circuit, heuristic_verification_stabilizers
 from .simulation import NoisyNDFTStatePrepSimulator
 
 __all__ = [
@@ -13,4 +13,5 @@ __all__ = [
     'gate_optimal_verification_stabilizers',
     'heuristic_verification_circuit',
     'NoisyNDFTStatePrepSimulator',
+    'heuristic_verification_stabilizers'
 ]
