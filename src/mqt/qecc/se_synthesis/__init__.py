6997f32716ddf0f4476f6205a40f568119aaa4f0
Tom Peham <pehamtom@gmx.at>
2024-07-23 09:37:51 +0200

    Removed files from wrong branch.

--- src/mqt/qecc/se_synthesis/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Optimal Construction of Decoding Circuits for QLDPC Codes."""
-
-from __future__ import annotations
-
-from .synthesize import OptimalSyndromeExtractionEncoder
-
-__all__ = [
-    "OptimalSyndromeExtractionEncoder",
-]
