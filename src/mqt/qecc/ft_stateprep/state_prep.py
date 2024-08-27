5e2ccfc8c357bab21d43c5eddeffeade1ea6924e
Tom Peham <pehamtom@gmx.at>
2024-08-27 09:07:00 +0200

    Remove incorrect offset.
    Computing  (d-1)//2 as the maximum number of errors is incorrect for codes with even distance.

--- src/mqt/qecc/ft_stateprep/state_prep.py
+++ src/mqt/qecc/ft_stateprep/state_prep.py
@@ -52,7 +52,7 @@ class StatePrepCircuit:
         self.z_checks = code.Hz.copy() if not zero_state else np.vstack((code.Lz.copy(), code.Hz.copy()))
 
         self.num_qubits = circ.num_qubits
-        self.max_errors = (code.distance - 1) // 2
+        self.max_errors = code.distance // 2
         self.x_fault_sets = [None for _ in range(self.max_errors + 1)]  # type: list[npt.NDArray[np.int8] | None]
         self.z_fault_sets = [None for _ in range(self.max_errors + 1)]  # type: list[npt.NDArray[np.int8] | None]
         self.x_fault_sets_unreduced = [None for _ in range(self.max_errors + 1)]  # type: list[npt.NDArray[np.int8] | None]
@@ -567,7 +567,7 @@ def gate_optimal_verification_stabilizers(
     Returns:
         A list of stabilizers to verify the state preparation circuit.
     """
-    max_errors = (sp_circ.code.distance - 1) // 2
+    max_errors = (sp_circ.code.distance) // 2
     layers = [[] for _ in range(max_errors)]  # type: list[list[npt.NDArray[np.int8]]]
     if max_ancillas is None:
         max_ancillas = sp_circ.max_z_measurements if x_errors else sp_circ.max_x_measurements
@@ -769,7 +769,7 @@ def heuristic_verification_stabilizers(
         additional_faults: Faults to verify in addition to the faults propagating in the state preparation circuit.
     """
     logging.info("Finding verification stabilizers using heuristic method")
-    max_errors = (sp_circ.code.distance - 1) // 2
+    max_errors = (sp_circ.code.distance) // 2
     layers = [[] for _ in range(max_errors)]  # type: list[list[npt.NDArray[np.int8]]]
     sp_circ.compute_fault_sets()
     fault_sets = (
@@ -950,13 +950,13 @@ def _measure_ft_stabs(
     measured_circ.compose(sp_circ.circ, inplace=True)
 
     if sp_circ.zero_state:
-        _measure_ft_z(measured_circ, z_measurements, t=(sp_circ.code.x_distance - 1) // 2)
+        _measure_ft_z(measured_circ, z_measurements, t=(sp_circ.code.x_distance) // 2)
         if full_fault_tolerance:
-            _measure_ft_x(measured_circ, x_measurements, flags=True, t=(sp_circ.code.x_distance - 1) // 2)
+            _measure_ft_x(measured_circ, x_measurements, flags=True, t=(sp_circ.code.x_distance) // 2)
     else:
-        _measure_ft_x(measured_circ, x_measurements, t=(sp_circ.code.z_distance - 1) // 2)
+        _measure_ft_x(measured_circ, x_measurements, t=(sp_circ.code.z_distance) // 2)
         if full_fault_tolerance:
-            _measure_ft_z(measured_circ, z_measurements, flags=True, t=(sp_circ.code.z_distance - 1) // 2)
+            _measure_ft_z(measured_circ, z_measurements, flags=True, t=(sp_circ.code.z_distance) // 2)
 
     return measured_circ
 
@@ -1182,8 +1182,8 @@ def _remove_trivial_faults(
     faults = faults.copy()
     logging.info("Removing trivial faults.")
     d_error = code.x_distance if x_errors else code.z_distance
-    t_error = (d_error - 1) // 2
-    t = (code.distance - 1) // 2
+    t_error = (d_error) // 2
+    t = (code.distance) // 2
     max_w = t_error // t
     for i, fault in enumerate(faults):
         faults[i] = _coset_leader(fault, stabs)
@@ -1234,7 +1234,7 @@ def naive_verification_circuit(sp_circ: StatePrepCircuit) -> QuantumCircuit:
 
     z_measurements = list(sp_circ.code.Hx)
     x_measurements = list(sp_circ.code.Hz)
-    reps = (sp_circ.code.distance - 1) // 2
+    reps = sp_circ.code.distance // 2
     return _measure_ft_stabs(sp_circ, z_measurements * reps, x_measurements * reps)
 
 
