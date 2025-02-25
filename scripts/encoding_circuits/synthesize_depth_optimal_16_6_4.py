34f07ac56aade9bf2b2effff00deae91dafea06b
Tom Peham <pehamtom@gmx.at>
2025-02-25 16:07:32 +0100

    Add script to synthesize depth-optimal 16_6_4.

---
+++ scripts/encoding_circuits/synthesize_depth_optimal_16_6_4.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+import numpy as np
+from qiskit import qasm2
+
+from mqt.qecc import CSSCode
+from mqt.qecc.circuit_synthesis import depth_optimal_encoding_circuit
+
+h = np.array([
+    [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1],
+    [0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1],
+    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+])
+
+l = np.array([
+    [1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+    [1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+    [1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+    [1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+])
+
+code = CSSCode(4, h, h)
+code.Lz = l
+code.Lx = l[[1, 0, 3, 2, 5, 4]]
+
+qc, enc = depth_optimal_encoding_circuit(code, min_depth=4, max_depth=8, min_timeout=300, max_timeout=7200)
+
+qc_str = qasm2.dumps(qc)
+with open("16_6_4_optimal.qasm", "w", encoding="utf-8") as f:
+    f.write(str(list(enc)))
+    f.write(qc_str)
