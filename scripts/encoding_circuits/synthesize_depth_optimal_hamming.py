3e7ce14af69bc2e0234e87b3e5d0c850cfe8c3db
Tom Peham <pehamtom@gmx.at>
2025-02-26 03:51:54 +0100

    Scripts.

--- scripts/encoding_circuits/synthesize_depth_optimal_hamming.py
+++ scripts/encoding_circuits/synthesize_depth_optimal_hamming.py
@@ -30,5 +30,5 @@ hamming_code.Lz = logicals
 qc = depth_optimal_encoding_circuit(hamming_code, min_depth=5, max_depth=14, min_timeout=300, max_timeout=7200)
 
 qc_str = qasm2.dumps(qc)
-with open("hamming_depth_optimal.qasm", "w", encoding="utf-8") as f:
-    f.write(qc_str)
+
+print(qc_str)
