23b54e6b8bdd1ef6d9a384b53c6ea15aeab4c102
Tom Peham <pehamtom@gmx.at>
2025-03-15 01:38:36 +0100

    Evaluation of linear cat state prep.

--- scripts/cat_states/estimate_error_rate.py
+++ scripts/cat_states/estimate_error_rate.py
@@ -3,10 +3,34 @@
 from __future__ import annotations
 
 import argparse
+from typing import TYPE_CHECKING
 
-from mqt.qecc.circuit_synthesis.cat_states import CatStatePreparationExperiment, cat_state_balanced_tree
+from mqt.qecc.circuit_synthesis.cat_states import CatStatePreparationExperiment, cat_state_balanced_tree, cat_state_line
+
+if TYPE_CHECKING:
+    from qiskit import QuantumCircuit
 
 permutations = {4: None, 8: [7, 1, 2, 3, 4, 5, 6, 0], 16: [0, 1, 6, 10, 13, 3, 5, 15, 2, 8, 11, 14, 4, 9, 12, 7]}
+
+
+def tree(w: int) -> tuple[QuantumCircuit, list[int]]:
+    """Create cat state preparation circuits for cat state preparation circuits with tree topology."""
+    if w not in {4, 8, 16}:
+        msg = "Weight must be 4, 8 or 16"
+        raise ValueError(msg)
+
+    circ = cat_state_balanced_tree(w)
+    perm = permutations[w]
+    return circ, circ, perm
+
+
+def line(w: int) -> tuple[QuantumCircuit, list[int]]:
+    """Create cat state preparation circuits for cat state preparation circuits with linear topology."""
+    circ = cat_state_line(w)
+    perm = list(range(w))
+    for i in range(1, w // 2, 2):
+        perm[i], perm[w // 2 + i] = perm[w // 2 + i], perm[i]
+    return circ, perm
 
 
 def main() -> None:
@@ -18,21 +42,17 @@ def main() -> None:
     parser.add_argument(
         "-n", "--n_samples", type=int, default=int(100e9), help="Number of samples to estimate logical error rate"
     )
+    parser.add_argument("-l", "--linear", type=bool, help="Whether to use linear or tree cat state preparation circuit")
 
     args = parser.parse_args()
-    if args.weight not in {4, 8, 16}:
-        msg = "Weight must be 4, 8 or 16"
-        raise ValueError(msg)
 
-    w = args.weight
+    if args.linear:
+        circ, perm = line(args.weight)
+    else:
+        circ, perm = tree(args.weight)
     p = args.p_error
     n = args.n_samples
-
-    circ = cat_state_balanced_tree(w)
-    circ_anc = cat_state_balanced_tree(w)
-    perm = permutations[w]
-
-    experiment = CatStatePreparationExperiment(circ, circ_anc, perm)
+    experiment = CatStatePreparationExperiment(circ, circ, perm)
     ra, ra_error, error_rates, error_rates_error = experiment.sample_cat_state(p, n)
 
     print(";".join([str(ra), str(ra_error), str(error_rates), str(error_rates_error)]))
