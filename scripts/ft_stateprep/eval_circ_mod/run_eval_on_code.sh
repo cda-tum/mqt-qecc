e05ba108499d01a0f0fc5accc730c07483f2bd6d
Erik Weilandt <erik.weilandt@tum.de>
2025-03-19 09:57:38 +0100

    add new headers for binomial errors in csv

--- scripts/ft_stateprep/eval_circ_mod/run_eval_on_code.sh
+++ scripts/ft_stateprep/eval_circ_mod/run_eval_on_code.sh
@@ -4,7 +4,7 @@
 
 declare -a p=("0.5" "0.3" "0.1" "0.09" "0.07" "0.05" "0.03" "0.01" "0.009" "0.007" "0.005" "0.003" "0.001" "0.0009" "0.0007" "0.0005" "0.0003" "0.0001")
 
-echo "p p_l acceptance errors runs" > "$1.csv"
+echo "p p_l acceptance errors runs p_l_error acceptance_error" > "$1.csv"
 
 run_and_write() {
     local res=$(python estimate_logical_error_rate.py ${@:2:$#-2} "-p" "${@: -1}")
