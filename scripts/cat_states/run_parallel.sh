e7f6704631d504ea2c71308c5cfcdf5dd07e5f06
Tom Peham <pehamtom@gmx.at>
2025-04-08 14:40:32 +0200

    Temporary script changes.

--- scripts/cat_states/run_parallel.sh
+++ scripts/cat_states/run_parallel.sh
@@ -2,7 +2,8 @@
 
 # Description: Estimate logical error rates for given code in parallel. Results are saved in a csv file described by the first argument. All other arguments are passed to the python script.
 
-declare -a p=("0.001" "0.002" "0.003" "0.004" "0.005" "0.006" "0.007" "0.008" "0.009" "0.01" "0.02" "0.03" "0.04" "0.05" "0.06" "0.07" "0.08" "0.09" "0.1" "0.2")
+# declare -a p=("0.001" "0.002" "0.003" "0.004" "0.005" "0.006" "0.007" "0.008" "0.009" "0.01" "0.02" "0.03" "0.04" "0.05" "0.06" "0.07" "0.08" "0.09" "0.1" "0.2")
+declare -a p=("0.007" "0.008" "0.009" "0.01" "0.02" "0.03")
 
 echo "p; rar; errors; errors_errors" > "$1.csv"
 
@@ -14,4 +15,4 @@ run_and_write() {
 
 export -f run_and_write
 
-parallel --load 16 --link run_and_write $@ ::: ${p[@]}
+parallel --load 2 --link run_and_write $@ ::: ${p[@]}
