168fb96b6b359f34d7c61c16b616e9fedf6e19bf
Erik Weilandt <erik.weilandt@tum.de>
2025-04-11 10:09:35 +0200

    remove unwanted csv files and change propabilities

--- scripts/ft_stateprep/eval_circ_mod/run_eval_on_code_mac.sh
+++ scripts/ft_stateprep/eval_circ_mod/run_eval_on_code_mac.sh
@@ -2,7 +2,8 @@
 
 # Description: Estimate logical error rates for given code in parallel. Results are saved in a csv file described by the first argument. All other arguments are passed to the python script.
 
-declare -a p=("0.01" "0.009" "0.007" "0.005" "0.003" "0.001" "0.0009" "0.0007")
+# declare -a p=("0.01" "0.009" "0.007" "0.005" "0.003" "0.001" "0.0009" "0.0007")
+declare -a p=("0.003" "0.0025" "0.0035" "0.002" "0.004" "0.0015" "0.0045" "0.001" "0.005" "0.0055" "0.006" "0.0065" "0.007" "0.0075" "0.008" "0.0085" "0.009" "0.0095" "0.01")
 
 echo "p p_l acceptance errors runs p_l_error acceptance_error" > "$1.csv"
 
